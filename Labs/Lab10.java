4e8c4dec0fd72651053f88958b4b9dbe63603997
zhe91060 <zhe91060@gmail.com>
2018-03-20 17:28:15 -0400

    Lab 10

---
+++ Labs/Lab10.java
@@ -0,0 +1,18 @@
+/*
+  Zhe Dong
+  Co Sci 290
+*/
+
+public class Lab10{
+  public static void main(String[] args){
+    System.out.println("Celsius Fahrenheit | Fahrenheit Celsius");
+    public static double celsiusToFahrenheit(double celsius){
+      fahrenheit = (9.0 / 5) * celsius + 32;
+    }
+    
+    public static double fahrenheitToCelsius(double fahrenheit){
+      celsius = (5.0 / 9) * (fahrenheit – 32);
+    }
+
+  }
+}
